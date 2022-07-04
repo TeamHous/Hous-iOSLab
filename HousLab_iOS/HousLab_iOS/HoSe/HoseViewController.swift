b9e2f5f8828e5b3bfa6275608717f725e5c39815
psychehose <psychehose@gmail.com>
2022-07-04 19:18:06 +0900

    ADD: add inject(hot reload), and Example code

--- HousLab_iOS/HousLab_iOS/HoSe/HoseViewController.swift
+++ HousLab_iOS/HousLab_iOS/HoSe/HoseViewController.swift
@@ -5,4 +5,40 @@
 //  Created by 김지현 on 2022/07/04.
 //
 
-import Foundation
+import UIKit
+
+final class HoseViewController: UIViewController {
+
+    private let helloworldLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello Hous"
+        label.backgroundColor = .white
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+
+
+    private func setupViews() {
+        view.addSubview(helloworldLabel)
+
+        NSLayoutConstraint.activate([
+            helloworldLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            helloworldLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
