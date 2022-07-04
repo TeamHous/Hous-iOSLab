e9b64db64d20785fb29b275fe87e214d25261bdb
Jihyun247 <wlguslove12345@gmail.com>
2022-07-04 12:08:13 +0900

    :tada: issue & PR 템플릿 세팅, 프로젝트 생성
    issue & PR 템플릿 세팅, 프로젝트 생성

---
+++ HousLab_iOS/HousLab_iOS/MainViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HousLab_iOS
+//
+//  Created by 김지현 on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
